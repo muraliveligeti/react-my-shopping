c9de80170b512cfe2ef7ce464f19e5da4c663b81
muraliveligeti <Administrator@HTIPL6206.china.huawei.com>
2019-10-20 08:58:07 +0800

    react shopping cart project

---
+++ src/home.component.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './home.styles.scss';
+
+const home = () =>(
+
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>HATS</h1>
+                    <span className='sub-title'>SHOPNOW</span>
+                </div>   
+            </div>  
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>HATS</h1>
+                    <span className='sub-title'>SHOPNOW</span>
+                </div>   
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>HATS</h1>
+                    <span className='sub-title'>SHOPNOW</span>
+                </div>   
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>HATS</h1>
+                    <span className='sub-title'>SHOPNOW</span>
+                </div>   
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>HATS</h1>
+                    <span className='sub-title'>SHOPNOW</span>
+                </div>   
+            </div>                                                  
+        </div>
+    </div>
+        
+)
+
+export default home;
